5dbd0647c8b9350b6f2c84560e121721acd27aa0
MiladinKi <miladinkovacevic12@gmail.com>
2024-11-20 18:19:44 +0100

    Initial commit

---
+++ src/main/java/zoo_tickets/ZooTicketsApplication.java
@@ -0,0 +1,13 @@
+package zoo_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zoo_tickets/ZooTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package zoo_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
